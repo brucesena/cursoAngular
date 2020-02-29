cd4b1101d50832f840d2064896de5716a8a2f81f
Bruce Cruz de Sena <bruce.sena@gmail.com>
2020-02-29 06:13:44 -0300

    novos arquivos

---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,13 @@
+import { Post } from './post.model';
+export class PostsService{
+  private posts: Post[] = [];
+
+  getPosts(){
+    return [...this.posts];
+  }
+
+  addPost (title, content:string){
+    const post: Post = {title: title, content: content};
+    this.posts.push(post);
+  }
+}
