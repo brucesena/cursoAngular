c43c08f96e784b27b3b7932ea4ebe1afc767b1e0
Bruce Cruz de Sena <bruce.sena@gmail.com>
2020-02-29 09:13:38 -0300

    Final da primeira parte deste curso

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,12 +1,32 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit, OnDestroy} from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent{
-  @Input() posts: Post[] = [];
+
+export class PostListComponent implements OnInit, OnDestroy{
+
+  posts: Post[];
+  private postsSub: Subscription;
+
+  constructor(public postService: PostsService){ }
+
+  ngOnInit(){
+    this.posts = this.postService.getPosts();
+    this.postsSub = this.postService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      })
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe;
+  }
+
 }
