3ec4a3b25318273f53ef1211825e7c42bd96e176
Bruce Cruz de Sena <bruce.sena@gmail.com>
2020-02-29 06:11:53 -0300

    Apenas atualizando

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,23 +1,12 @@
 import {Component, Input} from '@angular/core';
+
+import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent{
-  //@Input() posts;
-  posts = [
-    {title: 'Primeiro', content: 'aaaaaaaaaaa' },
-    {title: 'Segundo', content: 'bbbbbbbbbbb' },
-    {title: 'terceiro', content: 'cccccccccccccc' },
-    {title: 'quarto', content: 'dddddddddddddddd' },
-  ]
-
-
-
-  /*storedPosts = []
-  onPostAdded (post){
-  //  @Input() storedPosts = []
-  }*/
-
+  @Input() posts: Post[] = [];
 }
